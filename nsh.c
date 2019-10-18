f7bf4abc4c70f12346ff6d962ea683dd610d63a4
Vladislav Domin <sunspinx@gmail.com>
2019-10-18 17:59:37 +0200

    Update nsh.c

--- nsh.c
+++ nsh.c
@@ -4,6 +4,8 @@
 #include "kernel/fcntl.h"
 
 #define MAX_ARG 512
+#define OUTPUT (O_WRONLY | O_CREATE)
+#define INPUT O_RDONLY
 
 char buffer[MAX_ARG];
 char * args[MAX_ARG];
@@ -12,14 +14,8 @@ int args_length = 0;
 // Runs a command, args[0] = application
 void run_command(char ** args)
 {
-    int pid = fork();
-
-    if (pid > 0)
-        wait(0);
-    else if(pid == 0) {
-        exec(args[0], args);
-        fprintf(2, "Error: Unknown command <%s>.\n", args[0]);
-    }
+    exec(args[0], args);
+    fprintf(2, "Error: Unknown command <%s>.\n", args[0]);
 }
 
 void tokenize(char * buffer)
@@ -54,6 +50,104 @@ void tokenize(char * buffer)
     args[args_length] = 0;
 }
 
+void redirect(char * input, char * output)
+{
+    if(input != 0)
+    {
+        close(0);
+        int fd = open(input, INPUT);
+        if(fd < 0)
+            fprintf(2, "Error: Input redirection failed.\n");
+    }
+
+    if(output != 0)
+    {
+        close(1);
+        int fd = open(output, OUTPUT);
+        if(fd < 0)
+            fprintf(2, "Error: Output redirection failed.\n");
+    }
+}
+
+void parse(char ** args)
+{
+    char ** rightpipe = 0;
+    int haspipe = 0;
+    int hasredirect = 0;
+    char * input = 0;
+    char * output = 0;
+
+    for (int i = 0; i < args_length; i++)
+    {
+        if((strcmp(args[i], "|") == 0) && (args_length > i+1))
+        {
+            haspipe = 1;
+            rightpipe = args + i + 1;
+            args[i] = 0;
+        }
+        if((strcmp(args[i], "<") == 0) && (args_length > i+1))
+        {
+            hasredirect = 1;
+            input = args[i+1];
+            args[i] = 0;
+        }
+        if((strcmp(args[i], ">") == 0) && (args_length > i+1))
+        {
+            hasredirect = 1;
+            output = args[i+1];
+            args[i] = 0;
+        }
+    }
+
+    if(hasredirect == 1 && haspipe == 0)
+    {
+        int pid = fork();
+        if (pid == 0)
+        {
+            redirect(input,output);
+            run_command(args);
+        }
+        else
+        {
+            wait(0);
+        }
+    }
+
+    else if(haspipe == 1)
+    {
+        int fds[2];
+        pipe(fds);
+        int pid = fork();
+        if (pid == 0)
+        {
+            close(0);
+            dup(fds[0]);
+            close(fds[0]);
+            redirect(input,output);
+            run_command(args);
+        }
+        int pid2 = fork();
+        if (pid2 == 0)
+        {
+            close(1);
+            dup(fds[1]);
+            close(fds[1]);
+            run_command(rightpipe);
+        }
+        wait(0);
+        wait(0);
+    }
+
+    else
+    {
+        int pid = fork();
+        if (pid == 0)
+            run_command(args);
+        else
+            wait(0);
+    }
+}
+
 
 
 
@@ -67,7 +161,7 @@ int main() {
     {
         tokenize(buffer);
 
-        run_command(args);
+        parse(args);
 
         printf("@ ");
     }
